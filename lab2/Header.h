cd7d44906822fceaa4eda8573a1fcf1abe9f90bd
Karina Malij <karinamalij0303@gmail.com>
2026-02-18 15:57:29 +0200

    2

---
+++ lab2/Header.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+
+bool encode(const std::string& inputfile, const std::string& outputfile);
+
+bool decode(const std::string& inputfile, const std::string& outputfile);
